b6a6c3b7a88d26221854cd8d6033859cc566b934
brycetyf <bryce.tan.2018@smu.edu.sg>
2020-10-20 18:50:27 +0800

    Update musician.js

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
+  res.status('200').send("Status: All good after I changed it!");
 });
 
 // retrieve all musicians from data store
